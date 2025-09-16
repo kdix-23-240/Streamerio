7fff46cb590f7a746a56aef6c907c2f0a5552874
kdix-23-240 <akihide.3kami@gmail.com>
2025-09-17 00:40:31 +0900

    HPの増減を実装

---
+++ Streamerio_unity/Assets/Scripts/Game/Debug/DebugMonitor/AbilityButton.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityButton : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour _abilityComponent;
+    [SerializeField] private AbilityType _abilityType;
+    [SerializeField] private bool _isIncrease;
+    [SerializeField] private PlayerScriptableObject _playerData;
+
+    private IAbility _ability;
+
+    private void Awake()
+    {
+        // MonoBehaviourからIAbilityを取得
+        _ability = _abilityComponent as IAbility;
+    }
+
+    public void OnClick()
+    {
+        Count();
+    }
+
+    protected void Count()
+    {
+        if (_ability == null) return;
+
+        float amount = 0f;
+        switch (_abilityType)
+        {
+            case AbilityType.Hp:
+                amount = _playerData.HpDiff;
+                break;
+            case AbilityType.Power:
+                amount = _playerData.PowerDiff;
+                break;
+            case AbilityType.Speed:
+                amount = _playerData.SpeedDiff;
+                break;
+        }
+        
+        if (_isIncrease)
+        {
+            _ability.Increase(amount);
+        }
+        else
+        {
+            _ability.Decrease(amount);
+        }
+    }
+}
+
+public enum AbilityType
+{
+    Hp,
+    Power,
+    Speed
+}
