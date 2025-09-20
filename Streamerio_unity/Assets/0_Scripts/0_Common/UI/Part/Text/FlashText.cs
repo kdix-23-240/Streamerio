710926fa157caa2d649045b10ddffc958c07520f
guu <Yu0gura1624@outlook.jp>
2025-09-20 13:12:09 +0900

    ClearOverlay

---
+++ Streamerio_unity/Assets/0_Scripts/0_Common/UI/Part/Text/FlashText.cs
@@ -0,0 +1,48 @@
+using System.Threading;
+using Alchemy.Inspector;
+using Common.UI.Animation;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Common.UI.Part.Text
+{
+    /// <summary>
+    /// 点滅するテキスト
+    /// </summary>
+    public class FlashText: UIBehaviourBase
+    {
+        [SerializeField, LabelText("テキストの点滅アニメーション")]
+        private FlashAnimationComponentParam _flashAnimationParam;
+        
+        private FlashAnimationComponent _flashAnimation;
+
+        private CancellationTokenSource _cts;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            
+            _flashAnimation = new FlashAnimationComponent(CanvasGroup, _flashAnimationParam);
+        }
+        
+        /// <summary>
+        /// テキストのアニメーション再生
+        /// </summary>
+        public void PlayStartTextAnimation()
+        {
+            _cts = new CancellationTokenSource();
+            _flashAnimation.PlayAsync(_cts.Token).Forget();
+        }
+        
+        /// <summary>
+        /// テキストのアニメーションを止める
+        /// </summary>
+        public void StopStartTextAnimation()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            
+            CanvasGroup.alpha = _flashAnimationParam.MinAlpha;
+        }
+    }
+}
