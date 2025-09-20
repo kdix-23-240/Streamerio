5eb6bd885eec896dfbcf463d03ac1c7ae5c6480c
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-09-20 22:16:40 +0900

    部分的にアニメーションを修正

---
+++ Streamerio_unity/Assets/0_Scripts/1_InGame/Animation/NoAttackEnemyAnimation.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class NoAttackEnemyAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _idleAnimator;
+
+    void Start()
+    {
+        
+    }
+}
