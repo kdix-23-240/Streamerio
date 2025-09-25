80701e86da4710d390991fe4cad5f3b3233a4430
guu <Yu0gura1624@outlook.jp>
2025-09-25 22:57:56 +0900

    クリックのイベント共通化

--- Streamerio_unity/Assets/0_Scripts/0_Common/UI/Display/Background/DisplayBackgroundPresenter.cs
+++ Streamerio_unity/Assets/0_Scripts/0_Common/UI/Display/Background/DisplayBackgroundPresenter.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Threading;
 using Alchemy.Inspector;
+using Common.Audio;
+using Common.UI.Click;
 using Cysharp.Threading.Tasks;
 using R3;
+using R3.Triggers;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -11,41 +14,66 @@ namespace Common.UI.Display.Background
     /// <summary>
     /// UIの背景
     /// </summary>
-    [RequireComponent(typeof(DisplayBackgroundView))]
-    public class DisplayBackgroundPresenter: DisplayPresenterBase<DisplayBackgroundView>, IPointerClickHandler
+    [RequireComponent(typeof(DisplayBackgroundView), typeof(ObservableEventTrigger), typeof(ClickEventBinder))]
+    public class DisplayBackgroundPresenter: DisplayPresenterBase<DisplayBackgroundView>
     {
-        private Subject<Unit> _onClickEvent;
+        [SerializeField, ReadOnly]
+        private ObservableEventTrigger _clickTrigger;
+        [SerializeField, ReadOnly]
+        private ClickEventBinder _clickEventBinder;
+        
         /// <summary>
         /// 背景をクリックした時のイベント
         /// </summary>
-        public Observable<Unit> OnClickAsObservable => _onClickEvent;
+        public Observable<Unit> OnClickAsObservable => _clickEventBinder.ClickEvent;
+        
+        private CancellationTokenSource _cts;
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            
+            _clickTrigger ??= GetComponent<ObservableEventTrigger>();
+            _clickEventBinder ??= GetComponent<ClickEventBinder>();
+        }
+#endif
         
         /// <summary>
         /// 初期化
         /// </summary>
         public override void Initialize()
         {
-            _onClickEvent = new Subject<Unit>();
+            _clickEventBinder.Initialize();
             base.Initialize();
         }
-
+        
         public override async UniTask ShowAsync(CancellationToken ct)
         {
+            _clickEventBinder.BindClickEvent(_clickTrigger.OnPointerClickAsObservable());
             _IsShow = true;
             await View.ShowAsync(ct);
         }
         
         public override void Show()
         {
+            _clickEventBinder.BindClickEvent(_clickTrigger.OnPointerClickAsObservable());
             _IsShow = true;
             View.Show();
         }
-
-        public void OnPointerClick(PointerEventData eventData)
+        
+        public override async UniTask HideAsync(CancellationToken ct)
         {
-            _onClickEvent?.OnNext(Unit.Default);
+            await base.HideAsync(ct);
+            _clickEventBinder.Dispose();
         }
         
+        public override void Hide()
+        {
+            base.Hide();
+            _clickEventBinder.Dispose();
+        }
+
         /// <summary>
         /// 背景の有効/無効を設定
         /// </summary>
