9ff53c20ce195c5ee0eef3d6a6d8924d871b40cf
guu <Yu0gura1624@outlook.jp>
2025-10-07 23:05:18 +0900

    グローバルなライフスコープ設定

--- Streamerio_unity/Assets/0_Scripts/0_Common/LifeTimeScope/GlobalLifeTimeScope.cs
+++ Streamerio_unity/Assets/0_Scripts/0_Common/LifeTimeScope/GlobalLifeTimeScope.cs
@@ -1,16 +1,14 @@
-using Common.Audio;
 using Common.Save;
 using VContainer;
 using VContainer.Unity;
 
 namespace Common
 {
-    public class CommonLifeTimeScope: LifetimeScope
+    public class GlobalLifeTimeScope: LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<IVolumeSaveFacade, IPlayDataSaveFacade, SaveManager>(Lifetime.Singleton);
-
         }
     }
 }
