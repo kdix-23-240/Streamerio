17bd49636726c5a151a2eb6176927a08ae083a62
umiyuri777 <yuseiai15@gmail.com>
2025-10-16 01:10:54 +0900

    feat: アクセスするURLを管理するスクリプタブルオブジェクトを作成

---
+++ Streamerio_unity/Assets/0_Scripts/0_Common/Websocket/ApiConfigSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ApiConfigSO", menuName = "SO/Common/ApiConfigSO")]
+public class ApiConfigSO : ScriptableObject
+{
+    [Header("フロントのURLフォーマット")]
+    public string frontendUrlFormat = "https://streamerio.vercel.app/?streamer_id={0}";
+    
+    [Header("バックエンドのWebsocket用URL")]
+    public string backendUrl = "wss://streamario-web-backend-282618030957.asia-northeast1.run.app";
+    
+    [Header("バックエンドのHTTP用URL")]
+    public string backendHttpUrl = "https://streamario-web-backend-282618030957.asia-northeast1.run.app";
+}
