d3df8c6a7d2393721eda04fcf95221579d6105b7
guu <Yu0gura1624@outlook.jp>
2025-10-20 11:04:48 +0900

    Audioリファクタ、グローバルライフタイムスコープに適用

--- Streamerio_unity/Assets/0_Scripts/0_Common/LifeTimeScope/GlobalLifetimeScope.cs
+++ Streamerio_unity/Assets/0_Scripts/0_Common/LifeTimeScope/GlobalLifetimeScope.cs
@@ -2,21 +2,16 @@ using Common.Audio;
 using Common.QRCode;
 using Common.Save;
 using Common.UI.Dialog;
-using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 public class GlobalLifetimeScope : LifetimeScope
 {
-    [SerializeField]
-    private AudioMediator _audioMediator;
-    
     protected override void Configure(IContainerBuilder builder)
     {
         builder.Register<IVolumeSaveFacade, IPlayDataSaveFacade, SaveManager>(Lifetime.Singleton);
 
-        builder.RegisterComponent(_audioMediator);
-        builder.Register<IAudioFacade, AudioFacade>(Lifetime.Scoped);
+        builder.Register<IAudioFacade, AudioFacade>(Lifetime.Singleton);
         
         builder.Register<IQRCodeService, QRCodeService>(Lifetime.Singleton)
             .WithParameter(new QRCodeSpriteFactory());
