187a3251aadfa188523d5582c437e2284bf07d07
umiyuri777 <yuseiai15@gmail.com>
2025-10-20 15:08:06 +0900

    feat: Counter.Incrementで増やしたい数を指定できるようinterfaceを更新

--- Streamario_web_backend/pkg/counter/interface.go
+++ Streamario_web_backend/pkg/counter/interface.go
@@ -3,7 +3,7 @@ package counter
 // Counter: イベント回数 & 視聴者アクティビティを抽象化するインタフェース
 // すべてのメソッドは並行安全であること (goroutine から同時呼び出し想定)
 type Counter interface {
-    Increment(roomID, eventType string) (int64, error)        // カウント+1し現在値返す
+    Increment(roomID, eventType string, value int64) (int64, error)        // カウントをvalueだけ増やして現在のカウントを返す
     Get(roomID, eventType string) (int64, error)              // 現在カウント取得
     Reset(roomID, eventType string) error                     // カウントリセット(閾値到達後など)
     UpdateViewerActivity(roomID, viewerID string) error       // 視聴者アクティビティ更新(最終時刻記録)
