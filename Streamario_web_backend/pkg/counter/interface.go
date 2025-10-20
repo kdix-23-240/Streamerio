502ccc5a27fb21179d9a87ec3d3f22fbf7b952e2
umiyuri777 <yuseiai15@gmail.com>
2025-10-21 01:43:58 +0900

    chroe: インターフェースに超過分を設定するメソッドを追加

--- Streamario_web_backend/pkg/counter/interface.go
+++ Streamario_web_backend/pkg/counter/interface.go
@@ -6,6 +6,7 @@ type Counter interface {
     Increment(roomID, eventType string, value int64) (int64, error)        // カウントをvalueだけ増やして現在のカウントを返す
     Get(roomID, eventType string) (int64, error)              // 現在カウント取得
     Reset(roomID, eventType string) error                     // カウントリセット(閾値到達後など)
+    SetExcess(roomID, eventType string, excess int64) error   // 閾値超過分をカウントに設定（超過分を捨てない）
     UpdateViewerActivity(roomID, viewerID string) error       // 視聴者アクティビティ更新(最終時刻記録)
     GetActiveViewerCount(roomID string) (int64, error)        // 一定期間内のアクティブ視聴者数
 }
