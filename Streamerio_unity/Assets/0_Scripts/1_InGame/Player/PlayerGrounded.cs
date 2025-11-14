01b277b0ddef77cb773c29eaf28956158550f26f
kdix-23-240 <akihide.3kami@gmail.com>
2025-11-14 20:07:05 +0900

    魔法に触れると空中でジャンプできる問題を修正

--- Streamerio_unity/Assets/0_Scripts/1_InGame/Player/PlayerGrounded.cs
+++ Streamerio_unity/Assets/0_Scripts/1_InGame/Player/PlayerGrounded.cs
@@ -13,16 +13,19 @@ public class PlayerGrounded : MonoBehaviour
     
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Ground")) return;
         _isGrounded.Value = true;
     }
     
     public void OnTriggerStay2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Ground")) return;
         _isGrounded.Value = true;
     }
     
     public void OnTriggerExit2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Ground")) return;
         AudioManager.Instance.PlayAsync(SEType.PlayerJump, destroyCancellationToken).Forget();
         _isGrounded.Value = false;
     }
